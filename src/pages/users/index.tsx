ba01fd78be1f896be4ae7355ac0b38fa3245121e
Rone Silveira <ronebs10@hotmail.com>
2021-04-13 15:01:05 -0300

    creating random data to feed miraje api

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Text, Thead, Td, Th, Tr, useBreakpointValue } from '@chakra-ui/react'
 import Link from 'next/link';
+import { useEffect } from 'react';
 import { RiAddLine } from 'react-icons/ri';
 import { Header } from '../../components/Forms/Header'
 import { Pagination } from '../../components/Pagination';
@@ -10,6 +11,12 @@ export default function UserList() {
     base: false,
     lg: true,
   })
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }, [])
 
   return (
     <Box>
