a43da27aab93ca5bdd93ce04116f16c05075291a
Rone Silveira <ronebs10@hotmail.com>
2021-04-13 16:27:58 -0300

    rendering data from api and configuring ReactQueryDevTools

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,12 +1,37 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Text, Thead, Td, Th, Tr, useBreakpointValue } from '@chakra-ui/react'
 import Link from 'next/link';
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Text, Thead, Td, Th, Tr, useBreakpointValue, Spinner } from '@chakra-ui/react'
 import { useEffect } from 'react';
 import { RiAddLine } from 'react-icons/ri';
+import { useQuery } from 'react-query'
+
 import { Header } from '../../components/Forms/Header'
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar'
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () =>  {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      };
+    });
+
+    return users;
+    
+  }, {
+    staleTime: 1000 * 5,
+  })
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -41,36 +66,51 @@ export default function UserList() {
             
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                { isWideVersion && <Th>Data de cadastro</Th> }
-                <Th w="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}> 
-                <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Rone Silveira</Text>
-                    <Text fontSize="sm" color="gray.300">ronebs10@hotmail.com</Text>
-                  </Box>
-                </Td>
-                { isWideVersion && <Td>07 de abril de 2021</Td> }
-                
-              </Tr>
-             
-            </Tbody>
-          </Table>
+          { isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+              <Thead>
+                <Tr>
+                  <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                    <Checkbox colorScheme="pink" />
+                  </Th>
+                  <Th>Usuário</Th>
+                  { isWideVersion && <Th>Data de cadastro</Th> }
+                  <Th w="8"></Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {data.map(user => {
+                  return(
+                    <Tr key={user.id}>
+                      <Td px={["4", "4", "6"]}> 
+                        <Checkbox colorScheme="pink" />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold">{user.name}</Text>
+                          <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                        </Box>
+                      </Td>
+                      { isWideVersion && <Td>{user.createdAt}</Td> }
+                    </Tr>
+                  )
+                  
+                })}
+              </Tbody>
+            </Table>
 
-          <Pagination />
+              <Pagination />
+            </>
+            )}
         </Box>
 
       </Flex>
